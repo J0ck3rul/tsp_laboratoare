fba0788bfada81c4add1ca1eba78b1c848dde7a3
J0ck3rul <eduardtibuleac@gmail.com>
2020-03-24 15:17:56 +0200

    added lab 6

---
+++ lab6/lab6/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace lab6
+{
+    [DataContract(IsReference = true)]
+    public partial class Comment
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Text { get; set; }
+        [DataMember]
+        public int PostId { get; set; }
+        [DataMember]
+        public virtual Post Post { get; set; }
+    }
+}
